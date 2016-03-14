7add7b12207317826c996d34507f06c704b7c848
GPU User <gpu_user@k20c-1.georgiatech-metz.fr>
2016-03-14 11:00:00 +0100

    modification of plots in training

--- Network/python_training.py
+++ Network/python_training.py
@@ -24,40 +24,25 @@ solver.test_nets[0].forward()
 # we use a little trick to tile the first image (mosaique des images si batch size > 1)
 figure(1)
 imshow(solver.net.blobs['images'].data[:1, 0].transpose(1, 0, 2).reshape(240, 352),cmap='gray')
-show()
 print solver.net.blobs['labels'].data[:1]
 figure(2)
 imshow(solver.test_nets[0].blobs['images'].data[:1, 0].transpose(1, 0, 2).reshape(240,352),cmap='gray')
-show()
 print solver.test_nets[0].blobs['labels'].data[:1]
-
-# plotting conv1 layer before iterations begin
-figure(3)
-imshow(solver.net.params['conv1'][0].diff[:, 0].reshape(12,8, 11, 11).transpose(0, 2, 1, 3).reshape(12*11, 8*11),cmap='gray')
-show()
 
 # 1 Iteration to display conv 1 layer
 solver.step(1)
-figure(4)
+figure(3)
 imshow(solver.net.params['conv1'][0].diff[:, 0].reshape(12,8, 11, 11).transpose(0, 2, 1, 3).reshape(12*11, 8*11),cmap='gray')
-show()
-figure(5)
+figure(4)
 imshow(solver.test_nets[0].blobs['pool5'].data[:,0].reshape(5,7*10),cmap='gray')
-show()
 
-# # 10 iterations to display conv 1
-#for i in range(10):
-	#solver.step(1)
-#figure(5)
-#imshow(solver.net.params['conv1'][0].diff[:, 0].reshape(12,8, 11, 11).transpose(0, 2, 1, 3).reshape(12*11, 8*11),cmap='gray')
-#show()
-
+# Complete training
 max_iter = 200
 test_interval = 25
 # losses will also be stored in the log
 #test_acc = zeros(int(np.ceil(max_iter / test_interval)))
 train_loss = zeros(max_iter)
-output = zeros((max_iter, 8, 4)) # second argument is number of classes (4) and first is the number of examples to display/select (here we choose 8 first images)
+output = zeros((max_iter, 5, 4)) # second argument is number of classes (4) and first is the number of examples to display/select (here we choose 8 first images)
 # the main solver loop
 for it in range(max_iter):
 	solver.step(1)  # SGD by Caffe
@@ -66,7 +51,7 @@ for it in range(max_iter):
 	# store the output on the first test batch
 	# (start the forward pass at conv1 to avoid loading new data)
 	solver.test_nets[0].forward(start='conv1')
-	output[it] = solver.test_nets[0].blobs['fc8'].data[:8]
+	output[it] = solver.test_nets[0].blobs['fc8'].data[:5]
 	# run a full test every so often (Caffe can also do this for us and write to a log, but we show here how to do it directly in Python, where more complicated things are easier.)
 	if it % test_interval == 0:
 		print 'Iteration', it, 'testing...'
@@ -75,7 +60,7 @@ for it in range(max_iter):
 			solver.test_nets[0].forward()
 
 # Display conv1 layer after max_iter iterations:
-figure(6)
+figure(5)
 imshow(solver.net.params['conv1'][0].diff[:, 0].reshape(12,8, 11, 11).transpose(0, 2, 1, 3).reshape(12*11, 8*11),cmap='gray')
 show()
 
