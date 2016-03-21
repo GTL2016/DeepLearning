ce646a2165e4a537ce9560523dbcbfe27c44f3a3
yannraphael <yann.lif@gmail.com>
2016-03-21 11:21:41 +0100

    visu_data can now plot the test dataset and is faster than before

--- Database/visu_data.py
+++ Database/visu_data.py
@@ -10,25 +10,27 @@ import matplotlib.pyplot as plt
 file_name = "noname";
 if sys.argv[1]=='train':
 	file_name = "train.txt"
-elif sys.argv[1]=='val' or sys.argv[1]=='test':
+elif sys.argv[1]=='val':
 	file_name = "val.txt"
+elif sys.argv[1]=='test':
+	file_name = "test.txt"
 
 if file_name !="noname":
 	f=open(file_name,"r")
 	l = f.readlines()
 	L=[s.strip().split(' ') for s in l]
-
-	test = 0
-
+	
+	nb_lines = 0;
+	for s in L:
+		nb_lines = nb_lines+1;
+	
+	
+	i = 0
+	labels = np.zeros((nb_lines,4))
 	for s in L:
 		lab = np.array([[float(s[1]), float(s[2]), float(s[3]), float(s[4])]])
-		#print(lab)
-		if (test==0):
-			test = 1
-			labels = lab
-		else:
-			#labels = np.array([labels,lab])
-			labels = np.concatenate((labels,lab), axis=0)
+		labels[i,:]=lab
+		i=i+1
 
 	print(labels.shape)
 
@@ -40,4 +42,4 @@ if file_name !="noname":
 	# Show all figures
 	show()
 else:
-	print('Please state which dataset you want to plot (train or val)')
+	print('Please state which dataset you want to plot (train, val or test)')
