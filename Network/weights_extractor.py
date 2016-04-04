95903ee8158a11963731543bc314c35a278e757e
Cedric Pradalier <cedric.pradalier@georgiatech-metz.fr>
2016-04-04 19:42:44 +0200

    correction of the weight_extractor

--- Network/weights_extractor.py
+++ Network/weights_extractor.py
@@ -6,8 +6,8 @@ from caffe import layers as L
 from caffe import params as P
 import numpy as np
 
-train_val_path = 'train_val.prototxt'
-caffemodel_path = './snap/_iter_1000.caffemodel'
+train_val_path = '../../../classif/snap/caffenet_train9/train_val.prototxt'
+caffemodel_path = '../../../classif/snap/caffenet_train9/snap_iter_250000.caffemodel'
 weights_path = './weights'
 
 
@@ -19,15 +19,9 @@ elif sys.argv[1]=='gpu':
 # Load snapshot
 net = caffe.Net(train_val_path, caffemodel_path, caffe.TEST)
 net.forward()
-labels = net.blobs['labels'].data[:].transpose(0, 2, 1, 3).reshape(batch_size_test,4)
-pred = net.blobs['fc8'].data[:]
-for test_it in range(test_iter):
-	net.forward()
-	labels = np.concatenate((labels,net.blobs['labels'].data[:].transpose(0, 2, 1, 3).reshape(batch_size_test,4)))
-	pred = np.concatenate((pred,net.blobs['fc8'].data[:]))
 
-np.save(weights_path+'/conv1.npy',solver.net.params['conv1'][0].data)
-np.save(weights_path+'/conv2.npy',solver.net.params['conv2'][0].data)
-np.save(weights_path+'/conv3.npy',solver.net.params['conv3'][0].data)
-np.save(weights_path+'/conv4.npy',solver.net.params['conv4'][0].data)
-np.save(weights_path+'/conv5.npy',solver.net.params['conv5'][0].data)
+np.save(weights_path+'/conv1.npy',net.params['conv1'][0].data)
+np.save(weights_path+'/conv2.npy',net.params['conv2'][0].data)
+np.save(weights_path+'/conv3.npy',net.params['conv3'][0].data)
+np.save(weights_path+'/conv4.npy',net.params['conv4'][0].data)
+np.save(weights_path+'/conv5.npy',net.params['conv5'][0].data)
