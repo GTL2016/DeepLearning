0902f15b2e14b690f2b9d5f9b2cf16e8d6b3f573
yannraphael <yann.lif@gmail.com>
2016-04-10 16:15:52 +0200

    comments on make mean

--- Database/make_pfe_mean.sh
+++ Database/make_pfe_mean.sh
@@ -2,10 +2,12 @@
 
 DATA=./
 
-$TOOLS/compute_image_mean $DATA/train_rescaled_data_lmdb \
-  $DATA/train_data_mean.binaryproto
-
 $TOOLS/compute_image_mean $DATA/val_rescaled_data_lmdb \
   $DATA/val_data_mean.binaryproto
 
-echo "Done."
+echo "Val dataset done."
+
+$TOOLS/compute_image_mean $DATA/train_rescaled_data_lmdb \
+  $DATA/train_data_mean.binaryproto
+
+echo "Train dataset done."
